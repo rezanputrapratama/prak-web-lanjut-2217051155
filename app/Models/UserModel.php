73e92f0956fd014edb6e300fa0f16bb76b9c5789
rezanputrapratama <nsunna702@gmail.com>
2024-10-24 20:49:17 +0700

    Tugas 6

--- app/Models/UserModel.php
+++ app/Models/UserModel.php
@@ -8,15 +8,30 @@ use Illuminate\Database\Eloquent\Model;
 class UserModel extends Model
 {
     use HasFactory;
-    protected $table = 'user';
-    protected $guarded = ['id'];
 
-    public function kelas()
-    {
+    protected $table = 'user';
+    
+    protected $fillable = [
+        'nama',
+        'npm',
+        'kelas_id',
+        'foto',
+    ];
+
+    public function kelas(){
         return $this->belongsTo(Kelas::class, 'kelas_id');
     }
 
-    public function getUser(){
-        return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')->select('user.*', 'kelas.nama_kelas as nama_kelas')->get();
+    public function getUser($id = null) {
+        if ($id != null) {
+            return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                        ->select('user.*', 'kelas.nama_kelas')
+                        ->where('user.id', $id) 
+                        ->first(); 
+        } else {
+            return $this->join('kelas', 'kelas.id', '=', 'user.kelas_id')
+                        ->select('user.id', 'user.nama', 'user.npm', 'user.foto', 'kelas.nama_kelas')
+                        ->get();
+        }
     }
 }
